58de2088c37acffaf6b2d2e3efb3a20ee0cf6b7b
NamSam <namitasam05@gmail.com>
2024-12-23 22:36:59 +0530

    Create JavaCurrencyFormatter.java
    in progress

---
+++ JavaCurrencyFormatter.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.text.*;
+import java.lang.String;
+
+public class JavaCurrencyFormatter {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        String demo=payment+"";
+        StringBuilder fr;
+        for(int i=0;i<fr.length();i++){
+            if(demo.charAt(i)=='.')
+            {
+                if (demo.charAt(i) == '.') {
+        fr.setCharAt(i, ','); 
+            }
+        }
+        demo=demo/
+        String us="$"+demo;
+        String india="Rs."+demo;
+        String china="￥"+demo;
+        String france= fr+" €";
+        
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
